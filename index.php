4e4e2aaad2021f94f64869a3171018efae8f2418
Andrea <andr3z45@gmail.com>
2022-01-17 17:08:50 +0100

    prova esercizio

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$paragrafo = "PHP è un linguaggio di scripting interpretato, originariamente concepito per la programmazione di pagine web dinamiche. L'interprete PHP è un software libero distribuito sotto la PHP License.";
+$parolaCensurata = str_replace($_GET['parola'],"***",$paragrafo);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php bad words</title>
+</head>
+<body>
+    <h2>Paragrafo originale</h2>
+    <h3>Totale caratteri paragrafo: <?php echo strlen($paragrafo); ?></h3>
+    <p>
+        <?php echo $paragrafo;?>
+    </p>
+
+    <h2>Paragrafo censurato</h2>
+    <p>
+        <?php 
+        echo $paragrafo;
+        echo $parolaCensurata;
+        ?>
+
+    </p>
+    
+</body>
+</html>
